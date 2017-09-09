a6691c1aafcb9ddfb74b2a4a84a3fceaae3a2aa7
Matt <matt.latimer@gmail.com>
2017-09-09 19:58:50 +0000

    function now returns all bottles received through iterative recycling

--- bottles.js
+++ bottles.js
@@ -7,8 +7,7 @@
 */
 
 var cashIn = process.argv[2];
-var totalIn = {cash: cashIn, bottles: 0, caps: 0};
-
+var totalIn = {cash: cashIn, empties: 0, caps: 0};
 /*
   Function name: getBottles
   Input: one array of three numbers
@@ -19,23 +18,32 @@ var totalIn = {cash: cashIn, bottles: 0, caps: 0};
   bottles that can be redeemed after full recursive use
   of the recycling program
 */
+
 var getBottles = function(credit) {
-  var bottles = 0
+  var bottles = 0;
+  var empties = credit.empties;
+  var caps = credit.caps;
 
   if (credit.cash >= 2) {
     bottles += parseInt(credit.cash / 2);
-    var cashRemain = credit.cash % 2;
   }
-  if (credit.bottles >= 2) {
-    bottles += parseInt(credit.bottles / 2);
-    bottlesRemain = credit.bottles % 2;
+  if (credit.empties >= 2) {
+    bottles += parseInt(credit.empties / 2);
+    empties = credit.empties % 2;
   }
   if (credit.caps >= 4) {
     bottles += parseInt(credit.caps / 4);
-    capsRemain = credit.caps % 4;
+    caps = credit.caps % 4;
+  }
+
+  empties += bottles;
+  caps += bottles;
+
+  if (empties >= 2 || caps >= 4) {
+    bottles += getBottles({cash: 0, empties: empties, caps: caps});
   }
 
   return bottles;
 };
 
-console.log(getBottles(totalIn))
+console.log(getBottles(totalIn));
